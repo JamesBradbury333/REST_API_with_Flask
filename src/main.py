1a7b1b4051c60c1a308c26f3688f1bfeada4d3d0
James Bradbury <jamesbradbury222@gmail.com>
2022-01-08 22:28:44 +0000

    Add requirements.txt

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# REST API == Representational State Transfer - Application Programming Interface
+
+def main():
+    print("This is the main")
+    return
+
+if __name__ == "__main__":
+   main()
+
